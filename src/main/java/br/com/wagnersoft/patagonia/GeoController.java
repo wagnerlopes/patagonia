024ca8b239b8f2154103b75bdda9fe6ff5118a37
Wagner Lopes <wagnerlopes@gmail.com>
2026-09-06 16:38:33 -0300

    alteração menor

--- src/main/java/br/com/wagnersoft/patagonia/GeoController.java
+++ src/main/java/br/com/wagnersoft/patagonia/GeoController.java
@@ -1,16 +1,36 @@
 package br.com.wagnersoft.patagonia;
 
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class GeoController {
 
-    @GetMapping("/geo")
-    public String showBanner(Model model) {
-        model.addAttribute("geo", "xxx");
-        return "geo";
-    }
+  private static final Logger logger = LoggerFactory.getLogger(GeoController.class);
+
+  @Value("${app.usr}")
+  private String usr;
+
+  @GetMapping("/")
+  public String index(HttpSession session, Model model) throws IOException {
+    logger.info("+++ Index +++");
+    session.setMaxInactiveInterval(0);
+    session.setAttribute("usr", usr);
+    return "index";
+  }
+
+  @GetMapping("/geo")
+  public String showBanner(Model model) {
+    model.addAttribute("geo", "xxx");
+    return "geo";
+  }
 
 }
--- src/main/java/br/com/wagnersoft/patagonia/Patagonia.java
+++ src/main/java/br/com/wagnersoft/patagonia/Patagonia.java
@@ -1,17 +1,9 @@
 package br.com.wagnersoft.patagonia;
 
-import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import jakarta.servlet.http.HttpSession;
 
 /**
  * Patagonia starting application. 
@@ -20,25 +12,13 @@ import jakarta.servlet.http.HttpSession;
  * @version 1.0
  * @author Wagner Lopes
  */
-@Controller
 @SpringBootApplication
 public class Patagonia {
 
   private static final Logger logger = LoggerFactory.getLogger(Patagonia.class);
 
-  @Value("${app.usr}")
-  private String usr;
-  
-  @GetMapping("/")
-  public String index(HttpSession session, Model model) throws IOException {
-    logger.info("+++ Index +++");
-    session.setMaxInactiveInterval(0);
-    session.setAttribute("usr", usr);
-    return "index";
-  }
-
-  
   public static void main(String[] args) {
+    logger.info("Patagonia iniciando...");
     SpringApplication.run(Patagonia.class, args);
   }
 
